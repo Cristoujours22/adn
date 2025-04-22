d2eefca6b6600a184581096f7e43093a7ac1b740
Cristoujours22 <capaalonso@gmail.com>
2025-04-22 00:49:58 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Login';
+import './App.module.css';
+import App from './Login'; // Login
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App /> {/* Renderiza el componente Login */}
+    </div>
+
   </React.StrictMode>
 );
 
