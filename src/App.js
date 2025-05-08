d9cb536025522a06bebaf254d81033bd747dda9a
Cristoujours22 <capaalonso@gmail.com>
2025-05-07 22:21:18 -0500

    2.1.1.6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import styles from './App.module.css';
-import Menu from './menu';
+import React from 'react';
+import LoginPage from './Login';
 
 function App() {
-  const [modoOscuro, setModoOscuro] = useState(() => {
-    const saved = localStorage.getItem('darkMode');
-    return saved ? JSON.parse(saved) : false;
-  });
-
-  useEffect(() => {
-    if (modoOscuro) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }, [modoOscuro]);
-
-  return (
-    <div className={`${styles.App} ${modoOscuro ? styles['modo-oscuro'] : ''}`}>
-      <Menu setModoOscuro={setModoOscuro} modoOscuro={modoOscuro} />
-      <h1>¡Hola, mundo!</h1>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
